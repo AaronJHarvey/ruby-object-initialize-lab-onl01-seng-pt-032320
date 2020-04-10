489a5ff31a70764582c801eb465904f240446b0e
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-10 02:54:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,10 @@
 class Dog 
-  def initialize(name)
+  def initialize(name, breed)
     @name = name
+    @breed = breed
   end
   
-  def initialize(breed)
-    @breed = breed 
-  end
+
   
   attr_accessor :name 
   attr_accessor :breed 
